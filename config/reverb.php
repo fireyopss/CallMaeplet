8cff44b8f3c054511b7e3fcb874d1d91d20ed6aa
MrOrangeade <tayyabdev@outlook.com>
2025-03-25 13:09:11 +0000

    path

--- config/reverb.php
+++ config/reverb.php
@@ -31,7 +31,7 @@ return [
         'reverb' => [
             'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
             'port' => env('REVERB_SERVER_PORT', 8080),
-            'path' => env('REVERB_SERVER_PATH', ''),
+            'path' => env('REVERB_SERVER_PATH', '/reverb'),
             'hostname' => env('REVERB_HOST'),
             'options' => [
                 'tls' => [],
